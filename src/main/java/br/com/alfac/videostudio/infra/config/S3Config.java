42ff9f55c0e9055ebcfef507ca19a03166a463cd
carlohcs <carlohcs@gmail.com>
2025-02-01 19:18:26 -0300

    feat: garante que a região aws está definida corretamente

--- src/main/java/br/com/alfac/videostudio/infra/config/S3Config.java
+++ src/main/java/br/com/alfac/videostudio/infra/config/S3Config.java
@@ -1,5 +1,6 @@
 package br.com.alfac.videostudio.infra.config;
 
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +11,8 @@ import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-import org.mockito.Mockito;
 
 import java.net.URI;
 
@@ -24,7 +25,7 @@ public class S3Config {
     @Value("${aws.secretKey}")
     private String secretKey;
 
-    @Value("${aws.sessionToken}")
+    @Value("${aws.sessionToken:}")
     private String sessionToken;
 
     @Value("${cloud.aws.region.static}")
@@ -38,41 +39,18 @@ public class S3Config {
     public S3Client s3ClientLocal() {
         return S3Client.builder()
                 .region(Region.of(awsRegion))
-                .credentialsProvider(getCredentialsProvider())
+                .credentialsProvider(getCredentialsProvider("local"))
                 .endpointOverride(URI.create(s3Endpoint))
-                .serviceConfiguration(software.amazon.awssdk.services.s3.S3Configuration.builder()
-                        .pathStyleAccessEnabled(true).build())
+                .serviceConfiguration(S3Configuration.builder().pathStyleAccessEnabled(true).build())
                 .build();
     }
 
     @Bean
     @Profile("prod")
-    public S3Client s3Client() {
+    public S3Client s3ClientProd() {
         return S3Client.builder()
                 .region(Region.of(awsRegion))
-                .credentialsProvider(getCredentialsProvider())
-                .build();
-    }
-
-    @Bean
-    @Profile("local")
-    public S3Presigner s3PresignerLocal(S3Client s3ClientLocal) {
-        return S3Presigner.builder()
-                .endpointOverride(URI.create(s3Endpoint))
-                .region(s3ClientLocal.serviceClientConfiguration().region())
-                .credentialsProvider(getCredentialsProvider())
-                .serviceConfiguration(software.amazon.awssdk.services.s3.S3Configuration.builder()
-                        .pathStyleAccessEnabled(true) // Força o uso de path-style
-                        .build())
-                .build();
-    }
-
-    @Bean
-    @Profile("prod")
-    public S3Presigner s3Presigner(S3Client s3Client) {
-        return S3Presigner.builder()
-                .region(s3Client.serviceClientConfiguration().region())
-                .credentialsProvider(getCredentialsProvider())
+                .credentialsProvider(getCredentialsProvider("prod"))
                 .build();
     }
 
@@ -83,13 +61,35 @@ public class S3Config {
     }
 
     @Bean
+    @Profile("local")
+    public S3Presigner s3PresignerLocal() {
+        return S3Presigner.builder()
+                .endpointOverride(URI.create(s3Endpoint))
+                .region(Region.of(awsRegion))
+                .credentialsProvider(getCredentialsProvider("local"))
+                .serviceConfiguration(S3Configuration.builder()
+                        .pathStyleAccessEnabled(true) // Força o uso de path-style
+                        .build())
+                .build();
+    }
+
+    @Bean
+    @Profile("prod")
+    public S3Presigner s3PresignerProd() {
+        return S3Presigner.builder()
+                .region(Region.of(awsRegion))
+                .credentialsProvider(getCredentialsProvider("prod"))
+                .build();
+    }
+
+    @Bean
     @Profile("test")
     public S3Presigner s3PresignerTest() {
         return Mockito.mock(S3Presigner.class);
     }
 
-    private AwsCredentialsProvider getCredentialsProvider() {
-        if ("prod".equals(System.getProperty("spring.profiles.active"))) {
+    private AwsCredentialsProvider getCredentialsProvider(String profile) {
+        if ("prod".equals(profile)) {
             return StaticCredentialsProvider.create(
                     AwsSessionCredentials.create(accessKey, secretKey, sessionToken));
         } else {
